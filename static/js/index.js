cfc9fe70b4ab81ae0b469b69381a172c05f3af35
marmolpen3 <marmolpen3@alum.us.es>
2024-05-03 14:07:38 +0200

    add sigmoid function

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,21 @@
 "use strict";
 
 import { AutoModelForSequenceClassification, AutoTokenizer } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0";
-import { parseHTML } from "/static/js/utils/parseHTML.js";
 
 function softmax(arr) {
     return arr.map(function (value, index) {
         return Math.exp(value) / arr.map(function (y /*value*/) { return Math.exp(y) }).reduce(function (a, b) { return a + b })
     })
+}
+
+function sigmoid(arr) {
+    return arr.map((x) => 1 / (1 + Math.exp(-x)));
+}
+
+function parseHTML(str) {
+    let tmp = document.implementation.createHTMLDocument();
+    tmp.body.innerHTML = str;
+    return tmp.body.children[0];
 }
 
 async function handleAnalyseTerms(event) {
@@ -17,12 +26,13 @@ async function handleAnalyseTerms(event) {
     let terms = formData.get("terms")
     console.log(terms)
 
+
     // model
     let tokenizer = await AutoTokenizer.from_pretrained('marmolpen3/lexglue-unfair-tos-onnx', { quantized: false });
     let model = await AutoModelForSequenceClassification.from_pretrained('marmolpen3/lexglue-unfair-tos-onnx', { quantized: false });
     let input_ids = await tokenizer(terms);
     let outputs = await model(input_ids);
-    let normResults = softmax(outputs.logits.data)
+    let normResults = sigmoid(outputs.logits.data)
     console.log(normResults)
 
     // result 
@@ -39,30 +49,37 @@ async function handleAnalyseTerms(event) {
                 <p id="clause" class="card-text">${terms}</p>
             </div>
             <div class="card-footer text-muted">
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[0] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}%" >Limitation of liability</span>
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[1] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}">Unilateral termination</span>
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[2] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}">Unilateral change</span>
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[3] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}">Content removal</span>
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[4] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}">Contract by using</span>
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[5] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}">Choice of law</span>
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[6] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}">Jurisdiction</span>
-                <span class="me-2 badge bg-primary rounded-pill ${normResults[7] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
-                    title="${Math.round(normResults[0]*100)}">Arbitration</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[0] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[0] * 100).toFixed(2)}%" >Limitation of liability</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[1] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[1] * 100).toFixed(2)}%">Unilateral termination</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[2] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[2] * 100).toFixed(2)}%">Unilateral change</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[3] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[3] * 100).toFixed(2)}%">Content removal</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[4] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[4] * 100).toFixed(2)}%">Contract by using</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[5] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[5] * 100).toFixed(2)}%">Choice of law</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[6] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[6] * 100).toFixed(2)}%">Jurisdiction</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[7] < 0.5 ? "btn-opacity" : ""}" data-bs-toggle="tooltip"
+                    title="${(normResults[7] * 100).toFixed(2)}%">Arbitration</span>
             </div>
         </div>`;
 
     let card = parseHTML(html);
     results.appendChild(card);
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 }
 
 function main() {
+    console.log("hello world");
+
     let sendTos = document.getElementById("form-send-tos");
     sendTos.onsubmit = handleAnalyseTerms;
 }
--- static/js/utils/parseHTML.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function parseHTML(str) {
-    let tmp = document.implementation.createHTMLDocument();
-    tmp.body.innerHTML = str;
-    return tmp.body.children[0];
-}
-
-export { parseHTML };
