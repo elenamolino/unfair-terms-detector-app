14c086306bf92109eb0891dc31fe463bd83efbc2
marmolpen3 <marmolpen3@alum.us.es>
2024-05-03 11:52:24 +0200

    base app from react

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+function handleEditPhoto(event){
+    // call model
+}
+
+
+async function main() {
+    
+    let sendTos = document.querySelector("#tos-button");
+    sendTos.onclick = handleAnalyseTerms;
+
+}
+
+document.addEventListener("DOMContentLoaded", main);
