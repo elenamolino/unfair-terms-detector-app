8d2129001d7960cdf7ded5896e024a9f9447e293
marmolpen3 <marmolpen3@alum.us.es>
2024-05-03 13:49:25 +0200

    add model analysis

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,65 @@
 "use strict";
 
+import { AutoModelForSequenceClassification, AutoTokenizer } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0";
+import { parseHTML } from "/static/js/utils/parseHTML.js";
 
-function handleAnalyseTerms(event) {
-    // call model
+function softmax(arr) {
+    return arr.map(function (value, index) {
+        return Math.exp(value) / arr.map(function (y /*value*/) { return Math.exp(y) }).reduce(function (a, b) { return a + b })
+    })
+}
 
-    console.log("call model")
+async function handleAnalyseTerms(event) {
+    // form data
+    event.preventDefault();
+    let form = event.target;
+    let formData = new FormData(form);
+    let terms = formData.get("terms")
+    console.log(terms)
+
+    // model
+    let tokenizer = await AutoTokenizer.from_pretrained('marmolpen3/lexglue-unfair-tos-onnx', { quantized: false });
+    let model = await AutoModelForSequenceClassification.from_pretrained('marmolpen3/lexglue-unfair-tos-onnx', { quantized: false });
+    let input_ids = await tokenizer(terms);
+    let outputs = await model(input_ids);
+    let normResults = softmax(outputs.logits.data)
+    console.log(normResults)
+
+    // result 
+    let alert = document.getElementById("empty-alert");
+    alert.classList.add("visually-hidden");
+
     let results = document.getElementById("results");
+    results.classList.remove("visually-hidden");
+
+    let html = `
+        <div class="card mb-3">
+            <div class="card-body">
+                <p id="clause" class="card-text">${terms}</p>
+            </div>
+            <div class="card-footer text-muted">
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[0] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}%" >Limitation of liability</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[1] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}">Unilateral termination</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[2] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}">Unilateral change</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[3] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}">Content removal</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[4] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}">Contract by using</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[5] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}">Choice of law</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[6] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}">Jurisdiction</span>
+                <span class="me-2 badge bg-primary rounded-pill ${normResults[7] < 0.5 ? "btn-opacity" : "" }" data-bs-toggle="tooltip"
+                    title="${Math.round(normResults[0]*100)}">Arbitration</span>
+            </div>
+        </div>`;
+
+    let card = parseHTML(html);
     
+    results.appendChild(card);
 }
 
 function main() {
