e218f9e76d2f2f230a38b5d2f66688c2df547e29
marmolpen3 <marmolpen3@alum.us.es>
2024-11-07 02:30:22 +0100

    add agreement to playground

--- static/js/render/gallery.js
+++ static/js/render/gallery.js
@@ -25,16 +25,22 @@ const galleryRenderer = {
     },
     asCard: function (term) {
         let unfairTerms = term.clauses.filter(term => term.isUnfair).length;
+        let unfairness = ((unfairTerms/term.clauses.length)*100).toFixed(0)
         let html = `
         <div class="col-md-4">
-            <div class="card card-tam">
+            <div class="card card-tam mb-3">
                 <a href="term_detail.html?termId=${term.id}">    
                 <img src="${term.icon}" class="card-img-top">   
                 </a>         
                 <div class="card-body">
-                    <h5 class="card-title text-center">${term.title}</h5>
+                    <a href="${term.url}" target="_blank">  
+                        <h5 class="card-title text-center">${term.title}</h5>
+                    </a>
                     <p class="card-text">${term.date}</p>
-                    <p>Total Terms ${term.clauses.length} - Unfair Terms ${unfairTerms}</p>
+                    <p>Total Terms <span class="badge text-bg-primary">${term.clauses.length}</span> - Unfair Terms <span class="badge text-bg-danger">${unfairTerms}</span></p>
+                    <p>Percentage of unfairness: ${unfairness} %</p>
+                    <p>Number of sections: ${term.num_sections}</p>
+                    <p>Number of words: ${term.num_words}</p>
                 </div>
             </div>
         </div>`;
